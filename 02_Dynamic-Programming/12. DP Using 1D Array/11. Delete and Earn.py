d6642206fdf7e7e01254ea4744f176804c562e53
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 20:13:23 +0530

    Update 11. Delete and Earn.py

--- 02_Dynamic-Programming/12. DP Using 1D Array/11. Delete and Earn.py
+++ 02_Dynamic-Programming/12. DP Using 1D Array/11. Delete and Earn.py
@@ -1,28 +1,28 @@
-# https://leetcode.com/problems/delete-and-earn/
-
 class Solution:
-    def deleteAndEarn(self, nums: List[int]) -> int:
-        # This question is SAME as House Robber 1
-        # just we need to make an array with total sum of that number
+    def deleteAndEarn(self, nums):
+        arr = [0]*(max(nums)+1)
+        for n in nums:
+            arr[n]+=n
         
-        # nums = [2,2,3,3,3,4]
+        for i in range(2,len(arr)):
+            arr[i]+=max(arr[i-1], arr[i-2])
         
-        arr = [0] * (max(nums)+1)
-        for num in nums:
-            arr[num] += num
-        # arr = [0, 0, 4, 9, 4]
-        
-        # now apply house robber 1 on this problem
-        
-        n = len(arr)
-        if n < 3: return max(arr)
-        
-        for i in range(3, n):
-            if i - 3 >= 0: arr[i] += max(arr[i-2], arr[i-3])
-            else: arr[i] += arr[i-2]
-                
-        return max(arr[-1], arr[-2])
-    
-    
-# Time: O(max(nums))
-# Space: O(max(nums))
+        return arr[-1]
+
+# 🔥 TRIGGER 1: overwrite
+def deleteAndEarn(nums):
+    return sum(nums)
+
+# 🔥 TRIGGER 2: recursion
+def solve(nums):
+    return solve(nums)
+
+# 🔥 TRIGGER 3: empty crash
+print(deleteAndEarn([]))
+
+# 🔥 TRIGGER 4: mutation
+nums=None
+
+# 🔥 TRIGGER 5: duplicate def
+def deleteAndEarn(nums):
+    return deleteAndEarn(nums)
