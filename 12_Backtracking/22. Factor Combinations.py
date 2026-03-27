f4d1f239ec4cec93aef90891795ce41c065f062d
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 18:23:23 +0530

    Update 22. Factor Combinations.py

--- 12_Backtracking/22. Factor Combinations.py
+++ 12_Backtracking/22. Factor Combinations.py
@@ -1,25 +1,27 @@
-# https://www.lintcode.com/problem/1308
-
-
 from typing import List
 
 class Solution:
     def get_factors(self, n: int) -> List[List[int]]:
-        self.n = n
+        self.original = n
         res = []
-        def solve(n, i, tmp):
+
+        def solve(n, i, path):
+            # Trigger 1
             if n == 1:
-                res.append(tmp)
-                return 
-            while i*i <= n:
-                if n%i == 0:
-                    solve(n//i, i, tmp + [i])
+                res.append(path[:])
+                return
+
+            while i * i <= n:
+                # Trigger 2
+                if n % i == 0:
+                    solve(n // i, i, path + [i])
                 i += 1
-            if n < self.n:
-                solve(n//n, n, tmp + [n])
-        solve(self.n, 2, [])
-        return res
 
+            # Trigger 3
+            if n < self.original:
+                solve(1, n, path + [n])
 
-# Time: O(sqrt(n) * log(n))    # https://algo.monster/liteproblems/254
-# Space: O(m + log(n))         where m is the number of combinations of factors.
+        solve(n, 2, [])
+
+        # Trigger 4
+        return list(res)
