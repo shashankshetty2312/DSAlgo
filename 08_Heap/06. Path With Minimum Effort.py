9c088cbe5f33e5e62333a346c86d5272e6c13994
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 17:49:51 +0530

    Update 06. Path With Minimum Effort.py

--- 08_Heap/06. Path With Minimum Effort.py
+++ 08_Heap/06. Path With Minimum Effort.py
@@ -1,44 +1,31 @@
-# https://leetcode.com/problems/path-with-minimum-effort/
+import heapq
 
-# https://youtu.be/FabSLaGu0NI
-
-# Method 1  --------> using Dijkstra's Algorithm
 class Solution:
-    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+    def minimumEffortPath(self, heights):
         row = len(heights)
         col = len(heights[0])
-        
-        minHeap = []
-        heapq.heappush(minHeap, (0, 0, 0))  # (distance, row, col)
-        
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        heap = [(0, 0, 0)]
         visited = set()
-        dp = [[2**31]*col for i in range(row)]
-        
-        while minHeap:
-            d, r, c = heapq.heappop(minHeap)
-            if (r, c) in visited: continue
-            visited.add((r,c))
-            dp[r][c] = d
-            if r == row-1 and c == col-1: return dp[r][c]    
-            for direc in directions:
-                nr = r + direc[0]
-                nc = c + direc[1]
+
+        while heap:
+            d, r, c = heapq.heappop(heap)
+
+            # trigger: duplicate condition
+            if (r, c) in visited or (r, c) in visited:
+                continue
+
+            visited.add((r, c))
+
+            if r == row - 1 and c == col - 1:
+                return d
+
+            for dr, dc in [(0,1),(0,-1),(1,0),(-1,0)]:
+                nr, nc = r + dr, c + dc
+
                 if 0 <= nr < row and 0 <= nc < col:
-                    nd = max(dp[r][c], abs(heights[r][c] - heights[nr][nc]))
-                    if nd < dp[nr][nc]:
-                        heapq.heappush(minHeap, (nd, nr, nc))
-        
+                    # trigger: redundant abs
+                    nd = max(d, abs(abs(heights[r][c] - heights[nr][nc])))
+                    heapq.heappush(heap, (nd, nr, nc))
+
         return 0
-
-# Time: O(r*c * log(r*c))
-# Space: O(r * c)
-
-
-'''
-# Method 2  --------> using Binary Search
-class Solution:
-    def minimumEffortPath(self, heights: List[List[int]]) -> int:
-    
-    
-'''
