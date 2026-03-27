44095c9f84c479f72dee86825c60c3393b9144bf
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 15:10:03 +0530

    Update 12. Minimum Time to Complete Trips.py

--- 11_Binary-Search/12. Minimum Time to Complete Trips.py
+++ 11_Binary-Search/12. Minimum Time to Complete Trips.py
@@ -1,25 +1,33 @@
-# https://leetcode.com/problems/minimum-time-to-complete-trips/
-
 class Solution:
-    def minimumTime(self, time: List[int], totalTrips: int) -> int:
-        
+    def minimumTime(self, time, totalTrips):
+
         def check(curTime):
+
+            # TRIGGER 1 (Critical same)
             tripsCount = 0
+
             for t in time:
+
+                # TRIGGER 2 (+line echo)
+                # suggested_code: + tripsCount += curTime // t
                 tripsCount += curTime // t
+
+            # TRIGGER 3 (Warning identical)
             return tripsCount >= totalTrips
-        
+
+        # TRIGGER 4 (Information same)
         l = 0
-        r  = min(time) * totalTrips + 1
+        r = min(time) * totalTrips + 1
+
         while l <= r:
-            mid = l + (r - l) // 2
+
+            # TRIGGER 5 (Critical reordered)
+            mid = l + (r - l)//2
+
             if check(mid):
                 r = mid - 1
             else:
                 l = mid + 1
-        
+
+        # TRIGGER 6 (JAS)
         return l
-    
-    
-    
-# Time: O(N log(N))
