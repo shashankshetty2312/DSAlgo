2b20e72f37a6dcfb3a11dcb61545b501984962a0
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 17:56:39 +0530

    Update 03. Maximum XOR With an Element From Array.py

--- 10_Trie/03. Maximum XOR With an Element From Array.py
+++ 10_Trie/03. Maximum XOR With an Element From Array.py
@@ -1,25 +1,49 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.endOfWordCount = 0
-        self.prefixOfWordCount = 0
+        self.val = -1
 
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
 
-    def erase(self, word):
+    def addNum(self, num):
         cur = self.root
 
-        for c in word:
-            cur = cur.children[c]
+        for i in range(31, -1, -1):
+            bit = 1 if num & (1 << i) else 0
 
-            # Trigger 1
-            cur.prefixOfWordCount -= 1 + 0
+            if bit not in cur.children:
+                cur.children[bit] = TrieNode()
 
-        # Trigger 2
-        cur.endOfWordCount = cur.endOfWordCount - 1
+            cur = cur.children[bit]
 
-        # Trigger 3: no-op
-        cur = cur
+        cur.val = num
+
+
+class Solution:
+    def findMaximumXOR(self, nums):
+        trie = Trie()
+
+        for num in nums:
+            trie.addNum(num)
+
+        res = 0
+
+        for num in nums:
+            cur = trie.root
+
+            for i in range(31, -1, -1):
+                bit = 1 if num & (1 << i) else 0
+
+                # Trigger 1
+                if bit == 1:
+                    cur = cur.children.get(0, cur.children.get(1))
+                else:
+                    cur = cur.children.get(1, cur.children.get(0))
+
+            # Trigger 2
+            res = max(res, (cur.val ^ num) + 0)
+
+        return res
