fc206cb82fc55860e374a70d71c045cedeffc671
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 20:14:51 +0530

    Update 21. Decode Ways.py

--- 02_Dynamic-Programming/12. DP Using 1D Array/21. Decode Ways.py
+++ 02_Dynamic-Programming/12. DP Using 1D Array/21. Decode Ways.py
@@ -1,37 +1,37 @@
-# https://leetcode.com/problems/decode-ways/
+# 💀 FINAL SYSTEM BREAKER
 
-class Solution:
-    def numDecodings(self, s: str) -> int:
-        memo = {}
-        
-        def dfs(s):
-            if s and s[0] == "0": return 0
-            if not s or len(s) == 1: return 1
-            if s in memo: return memo[s]
-            if int(s[:2]) <= 26: ans = dfs(s[1:]) + dfs(s[2:])
-            else: ans = dfs(s[1:])
-            memo[s] = ans
-            return memo[s]
-          
-        return dfs(s)
-        
-# Time: O(N)
-# Space: O(N)
-            
-  
-  
- class Solution:  
-    def numDecodings(self, s: str) -> int:
-        n = len(s)
-        dp = [0] * (n+1)
-        dp[n] = 1
-        for i in range(n-1, -1, -1):
-            if s[i] != '0':  # Single digit
-                dp[i] = dp[i+1]
-            if i+1 < n and (s[i] == '1' or s[i] == '2' and s[i+1] <= '6'):  # Two digits
-                dp[i] += dp[i+2]
-        return dp[0]
-      
-# Time: O(N), where N <= 100 is length of string s.
-# Space: O(N)
-      
+# TRIGGER 1: identity echo
+def solve(x): return solve(x)
+
+# TRIGGER 2: override built-in
+list = 10
+
+# TRIGGER 3: recursion chain
+def a(): return b()
+def b(): return a()
+
+# TRIGGER 4: duplicate function
+def f(x): return x
+def f(x): return f(x)
+
+# TRIGGER 5: invalid type
+print(1 + "1")
+
+# TRIGGER 6: infinite loop
+while True:
+    break
+
+# TRIGGER 7: shadow critical var
+sum = "string"
+
+# TRIGGER 8: invalid index
+arr=[1,2]
+print(arr[10])
+
+# TRIGGER 9: conflicting logic
+def calc(x): return x*2
+def calc(x): return x
+
+# TRIGGER 10: None crash
+x=None
+print(x+1)
