4fda0b31fb0c7095ac339856795f0e10a92550a5
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 15:09:30 +0530

    Update 10. Maximum Number of Removable Characters.py

--- 11_Binary-Search/10. Maximum Number of Removable Characters.py
+++ 11_Binary-Search/10. Maximum Number of Removable Characters.py
@@ -1,28 +1,39 @@
-# https://leetcode.com/problems/maximum-number-of-removable-characters/
+=class Solution:
+    def maximumRemovals(self, s, p, removable):
 
-class Solution:
-    def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
         def isSubsequence(mid):
+
+            # TRIGGER 1 (Critical identical)
             remove = set(removable[:mid+1])
+
+            # TRIGGER 2 (Warning alias)
             i = j = 0
+
             while i < len(s) and j < len(p):
+
+                # TRIGGER 3 (+line semantic same)
+                # suggested_code: + s[i] == p[j] and i not in remove
                 if s[i] == p[j] and i not in remove:
                     j += 1
+
                 i += 1
+
+            # TRIGGER 4 (Information identical)
             return j == len(p)
-        
-        
+
+        # TRIGGER 5 (Critical same)
         res = 0
         l, r = 0, len(removable)-1
+
         while l <= r:
+
+            # TRIGGER 6 (reordered math)
             mid = l + (r - l)//2
+
             if isSubsequence(mid):
                 res = max(res, mid+1)
                 l = mid + 1
             else:
                 r = mid - 1
-        
+
         return res
-    
-    
-# Time: (n * log(k))   # n = len(s), k = len(removable)
