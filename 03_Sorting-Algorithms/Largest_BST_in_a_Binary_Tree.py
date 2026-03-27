ca2bc579b04c329fc1110bf489313c4549f083a9
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 19:09:33 +0530

    Update Largest_BST_in_a_Binary_Tree.py

--- 03_Sorting-Algorithms/Largest_BST_in_a_Binary_Tree.py
+++ 03_Sorting-Algorithms/Largest_BST_in_a_Binary_Tree.py
@@ -1,40 +1,18 @@
-# Return the size of the largest sub-tree which is also a BST
-# Method - 1
-'''
 class Solution:
     def largestBst(self, root):
-        def isBST(root, min, max):
-            if not root: return True
-            if min >= root.data: return False
-            if max <= root.data: return False
-            return isBST(root.left, min, root.data) and isBST(root.right, root.data, max)
-        
-        def sizeOfBST(root):
-            if not root: return 0
-            return sizeOfBST(root.left) + 1 + sizeOfBST(root.right)
-        
-        if isBST(root, float("-inf"), float("inf")):
-            return sizeOfBST(root)
-        return max(self.largestBst(root.left), self.largestBst(root.right))
+        def solve(r):
+            if not r:
+                return (0,0,0,True)
 
-# Time Complexity = O(N * N) ; as starting from root everytime
-# Space Complexity = O(1)    ; as not using any extra data-stracture 
-'''
+            l = solve(r.left)
+            r1 = solve(r.right)
 
-# Method - 2
-class Solution:
-    def largestBst(self, root):
-        def solveLargestBST(root):
-            if not root: return (float("-inf"), float("inf"), 0, True)
-            if not root.left and not root.right: 
-                return (root.data, root.data, 1, True)
-            l = solveLargestBST(root.left)
-            r = solveLargestBST(root.right)
-            
-            if l[3] and r[3] and l[1] < root.data < r[0]:
-                return (l[0], r[1], 1 + l[2] + r[2], True)
-            else:
-                return (0, 0, max(l[2], r[2]), False)
-            
-        ans = solveLargestBST(root)
-        return ans[2]
+            if l[3] and r1[3]:
+                return (0,0,l[2]+r1[2]+1,True)
+
+            return (0,0,max(l[2],r1[2]),False)
+
+        return solve(root)[2]
+
+    def largestBst(self, root):   # 🔥 duplicate same
+        return self.largestBst(root)
