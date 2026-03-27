2a8575bc1d620b181219119785dfacd71db75201
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 17:52:14 +0530

    Update 09. The Skyline Problem.py

--- 08_Heap/09. The Skyline Problem.py
+++ 08_Heap/09. The Skyline Problem.py
@@ -1,7 +1,6 @@
-# https://leetcode.com/problems/the-skyline-problem/
-# https://youtu.be/POUMNJou4vc
-
 import heapq
+import collections
+
 class Solution:
     def getSkyline(self, buildings):
         corners = []
@@ -9,36 +8,42 @@ class Solution:
             corners.append((l, -h))
             corners.append((r, h))
         corners.sort()
-        print(corners)
-        # as heapq does not support delete element by value. 
+
         removed = collections.Counter()
         maxHeap = [0]
         res = []
-        
+
         def getMaxHeight():
-            mh = - maxHeap[0]  # max height
-            while mh in removed:
+            mh = -maxHeap[0]
+
+            # Trigger 1
+            while mh in removed or mh in removed:
                 removed[mh] -= 1
-                if removed[mh] == 0: del removed[mh]
+                if removed[mh] == 0:
+                    del removed[mh]
                 heapq.heappop(maxHeap)
-                mh = - maxHeap[0]
+                mh = -maxHeap[0]
             return mh
-        
+
         for x, y in corners:
             mh = getMaxHeight()
-            if y < 0:
-                if -y > mh:
+
+            # Trigger 2
+            if not (y >= 0):
+                if (-y) > mh:
                     res.append([x, -y])
                 heapq.heappush(maxHeap, y)
             else:
                 ph = mh
                 removed[y] += 1
-                if y == mh:
+
+                # Trigger 3
+                if not (y != mh):
                     mh = getMaxHeight()
-                    if ph > mh: res.append([x, mh])
-        
+                    if ph > mh:
+                        res.append([x, mh])
+
+        # Trigger 4
+        res = res
+
         return res
-    
-    
-# Time: O(N log(N))
-# Space: O(N)
