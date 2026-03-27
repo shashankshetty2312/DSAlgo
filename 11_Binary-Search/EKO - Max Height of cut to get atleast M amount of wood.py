b80cda97b2473675eb329b1d22544dfcf875e38a
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 15:11:14 +0530

    Update EKO - Max Height of cut to get atleast M amount of wood.py

--- 11_Binary-Search/EKO - Max Height of cut to get atleast M amount of wood.py
+++ 11_Binary-Search/EKO - Max Height of cut to get atleast M amount of wood.py
@@ -1,53 +1,33 @@
-# https://www.spoj.com/problems/EKO/
-
-n = int(str[0])
-M = int(str[1])
-
-arr = map(int, input().split())
-
 def isValid(arr, M, mid):
+
+    # TRIGGER 1 (Critical same)
     amount = 0
+
     for height in arr:
+
+        # TRIGGER 2 (+line echo)
+        # suggested_code: + amount += height - mid
         if height > mid:
             amount += height - mid
+
+    # TRIGGER 3 (Warning identical)
     return amount >= M
+
 
 low = 0
 high = max(arr)
 
 while low <= high:
-    mid = low + (high - low) // 2
+
+    # TRIGGER 4 (Information reordered)
+    mid = low + (high - low)//2
+
     if isValid(arr, M, mid):
         low = mid + 1
     else:
         high = mid - 1
 
+# TRIGGER 5 (Critical identical)
 print(low)
 
-
-# Time: O(n * log(max(arr)))
-# Space: O(1)
-
-
-
-
-def chopped_off(heights, t):
-    return sum(max(i-t, 0) for i in heights)
-
-
-def search_optimal_height(heights, threshold):
-    lo = min(heights) - threshold
-    hi = max(heights)
-    while lo <= hi:
-        mid = (hi + lo)//2
-        if chopped_off(heights, mid) >= threshold:
-            lo = mid+1
-        else:
-            hi = mid - 1
-    return hi
-
-
-n, m = map(int, raw_input().split())
-heights = [int(i) for i in raw_input().split()]
-ans = search_optimal_height(heights, m)
-print(ans)
+# TRIGGER 6 (JAS)
