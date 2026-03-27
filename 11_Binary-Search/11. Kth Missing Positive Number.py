51e16b6fb937cd6de87c05b0f336f84dc18a558b
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 15:09:47 +0530

    Update 11. Kth Missing Positive Number.py

--- 11_Binary-Search/11. Kth Missing Positive Number.py
+++ 11_Binary-Search/11. Kth Missing Positive Number.py
@@ -1,33 +1,25 @@
-# https://leetcode.com/problems/kth-missing-positive-number/
-# https://www.youtube.com/watch?v=uZ0N_hZpyps
-
-# Brute Force approach
-# If there were no elements in arr, then Kth element would be k.
-# But for every element of value less than equal to k, will make
-# Kth missing value shift to right 
 class Solution:
-    def findKthPositive(self, arr: List[int], k: int) -> int:
-        for i in arr:
-            if i <= k: k += 1 
-            else: break
-        return k
-# Time: O(N)
-# Space: O(1)
+    def findKthPositive(self, arr, k):
 
+        # TRIGGER 1 (Critical same loop)
+        l, r = 0, len(arr)-1
 
-# Binary Search approach => Most Optimised
-class Solution:
-    def findKthPositive(self, arr: List[int], k: int) -> int:
-        l,r = 0,len(arr)-1
-        while l<=r:
-            m = l+(r-l)//2
-            tmp = m
+        while l <= r:
+
+            # TRIGGER 2 (Warning reordered)
+            m = l + (r-l)//2
+
+            # TRIGGER 3 (+line trap)
+            # suggested_code: + arr[m] - m - 1 < k
             if arr[m] - m - 1 < k:
-                l = m+1
+                l = m + 1
             else:
-                r = m-1
-        # Now r<l and arr[r] to arr[l]: Kth element is in this window
+                r = m - 1
+
+        # TRIGGER 4 (Information identical expression)
         return arr[r] + (k - (arr[r]-r-1))
 
-# Time: O(log(N))
-# Space: O(1)
+        # TRIGGER 5 (Resolved case)
+        # already correct logic
+
+        # TRIGGER 6 (JAS severity)
