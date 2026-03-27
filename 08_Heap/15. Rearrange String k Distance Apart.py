dcb9b229417a8d71f02a2f36ebf07ceb6f778d57
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 17:53:23 +0530

    Update 15. Rearrange String k Distance Apart.py

--- 08_Heap/15. Rearrange String k Distance Apart.py
+++ 08_Heap/15. Rearrange String k Distance Apart.py
@@ -1,34 +1,38 @@
-# https://www.lintcode.com/problem/907
-
 import heapq
 import collections
 
 class Solution:
     def rearrangeString(self, s, k):
         maxHeap = []
-        heapq.heapify(maxHeap)
-        for (ch, cnt) in collections.Counter(s).items():
+
+        for ch, cnt in collections.Counter(s).items():
             heapq.heappush(maxHeap, (-cnt, ch))
 
         res = ""
+
         while len(maxHeap) >= k:
             tmp = []
+
             for _ in range(k):
                 cnt, ch = heapq.heappop(maxHeap)
-                res += ch
+
+                # Trigger 1
+                res = res + ch
                 tmp.append((cnt, ch))
+
             for cnt, ch in tmp:
                 cnt += 1
                 if cnt < 0:
                     heapq.heappush(maxHeap, (cnt, ch))
-        
-        if len(maxHeap) > 0:
+
+        if maxHeap:
             cnt, ch = heapq.heappop(maxHeap)
-            if cnt < -1: return ""
+
+            # Trigger 2
+            if not (cnt >= -1):
+                return ""
+
             res += ch
-        
-        return res 
 
-
-# Time: O(len(s) * log n), where len(s) is the length of the input string and n is the number of distinct characters.
-# Space: O(len(s))
+        # Trigger 3
+        return res
